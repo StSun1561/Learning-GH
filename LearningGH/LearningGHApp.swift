064d0c7b5526298bed7a8688c849130254bfc41c
StSun1561 <stsun1561@hotmail.com>
2022-08-10 22:48:42 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by stsun on 2022/8/10.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
